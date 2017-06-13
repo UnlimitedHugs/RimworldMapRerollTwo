4e54eddcf349de3e3f6ffb53272e3c45eda5c337
UnlimitedHugs <unlimited.hugs@gmail.com>
2017-06-13 19:31:53 +0400

    Added monument building with basic graphics

---
+++ Source/GraphicData_Monument.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using Verse;
+
+namespace Reroll2 {
+	public class GraphicData_Monument : GraphicData {
+		public Vector2 drawOffset;
+		public Vector2 baseDrawSize = Vector2.one;
+		public Vector2 diceDrawSize = Vector2.one;
+		public ShaderType baseShaderType;
+		public ShaderType glowShaderType;
+		public string baseTexPath;
+		public string diceTexPath;
+		public string diceRadialTexPath;
+		public string diceGlowTexPath;
+		public string diceGlowRadialTexPath;
+
+		public GraphicData_Monument() {
+			graphicClass = typeof(Graphic_Monument);
+		}
+
+		/*public Texture2D BaseTex { get; private set; }
+		public Texture2D DiceTex { get; private set; }
+		public Texture2D DiceRadialTex { get; private set; }
+		public Texture2D DiceGlowTex { get; private set; }
+		public Texture2D DiceGlowRadialTex { get; private set; }*/
+		
+		/*public void PostLoad() {
+			texPath = baseTexPath;
+			LongEventHandler.ExecuteWhenFinished(() => {
+				BaseTex = ContentFinder<Texture2D>.Get(baseTexPath);
+				DiceTex = ContentFinder<Texture2D>.Get(diceTexPath);
+				DiceRadialTex = ContentFinder<Texture2D>.Get(diceRadialTexPath);
+				DiceGlowTex = ContentFinder<Texture2D>.Get(diceGlowTexPath);
+				DiceGlowRadialTex = ContentFinder<Texture2D>.Get(diceGlowRadialTexPath);
+			});
+		}*/
+	}
+}
---
+++ Source/Graphic_Monument.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using UnityEngine;
+using Verse;
+
+namespace Reroll2 {
+	public class Graphic_Monument : Graphic {
+		private new GraphicData_Monument data;
+
+		private Material BaseMat { set; get; }
+		private Material DiceMat { get; set; }
+		private Material DiceRadialMat { get; set; }
+		private Material DiceGlowMat { get; set; }
+		private Material DiceGlowRadialMat { get; set; }
+
+		public override void Init(GraphicRequest req) {
+			data = req.graphicData as GraphicData_Monument;
+			if (data == null) {
+				throw new Exception("Graphic_Monument requires GraphicData_Monument data type");
+			}
+			drawSize = req.drawSize;
+			InitMaterials(req);
+		}
+
+		public override Material MatSingle {
+			get { return BaseMat; }
+		}
+
+		private void InitMaterials(GraphicRequest req) {
+			var baseShader = ShaderDatabase.ShaderFromType(data.baseShaderType);
+			BaseMat = MaterialPool.MatFrom(data.baseTexPath, baseShader, req.color);
+			DiceMat = MaterialPool.MatFrom(data.diceTexPath, baseShader, req.color);
+			DiceRadialMat = MaterialPool.MatFrom(data.diceRadialTexPath, baseShader, req.color);
+			var glowShader = ShaderDatabase.ShaderFromType(data.glowShaderType);
+			DiceGlowMat = MaterialPool.MatFrom(data.diceGlowTexPath, glowShader, Color.white);
+			DiceGlowRadialMat = MaterialPool.MatFrom(data.diceGlowRadialTexPath, glowShader, Color.white);
+		}
+		
+		public override void DrawWorker(Vector3 loc, Rot4 rot, ThingDef thingDef, Thing thing) {
+			var rotation = QuatFromRot(rot);
+			loc = new Vector3(loc.x + data.drawOffset.x, loc.y, loc.z + data.drawOffset.y);
+			var baseMesh = MeshPool.GridPlane(data.baseDrawSize);
+			Graphics.DrawMesh(baseMesh, loc, rotation, BaseMat, 0);
+			var diceMesh = MeshPool.GridPlane(data.diceDrawSize);
+			Graphics.DrawMesh(diceMesh, loc + Altitudes.AltIncVect, rotation, DiceMat, 0);
+			Graphics.DrawMesh(diceMesh, loc + Altitudes.AltIncVect*2f, rotation, DiceGlowMat, 0);
+			if (ShadowGraphic != null) {
+				ShadowGraphic.DrawWorker(loc, rot, thingDef, thing);
+			}
+		}
+	}
+}
